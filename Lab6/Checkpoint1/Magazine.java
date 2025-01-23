f569cf8918e28a0cafe9c87af4b7e6af7380c606
Phutawa <phutawanmuangma@gmail.com>
2025-01-23 09:33:04 +0700

    Add files via upload

---
+++ Lab6/Checkpoint1/Magazine.java
@@ -0,0 +1,27 @@
+package Checkpoint1;
+
+    public class Magazine extends Publication {
+        
+        private String Unit;
+
+    public Magazine(){
+
+    }
+    public Magazine(String Publisher, int Pages, Double Price, String Title, String Unit) {
+        super(Publisher, Pages, Price, Title); 
+        this.Unit = Unit;
+    }
+    
+    public String GetUnit(){
+        return Unit;
+    }
+    public void SetUnit(String Unit){
+        this.Unit = Unit;
+    }
+   
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("This Magazine is " + GetUnit() + " Print");
+    }
+}
