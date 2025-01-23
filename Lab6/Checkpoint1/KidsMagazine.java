c3d20511cd90bf6afee032a1bfc5464538600d44
Phutawa <phutawanmuangma@gmail.com>
2025-01-23 13:58:03 +0700

    Add files via upload

---
+++ Lab6/Checkpoint1/KidsMagazine.java
@@ -0,0 +1,28 @@
+package Checkpoint1;
+
+public class KidsMagazine extends Publication {
+    
+    private int AgeReccom;
+
+    public KidsMagazine(){
+        super();
+        AgeReccom = 0 ;
+
+    }
+    public KidsMagazine(String Publisher, int Pages, Double Price, String Title ,int AgeReccom){
+        super(Publisher, Pages, Price, Title);
+        this.AgeReccom = AgeReccom;
+    }
+    public int GetAgeReccom(){
+        return AgeReccom;
+    }
+    public void SetAgeReccom(int AgeReccom){
+        this.AgeReccom = AgeReccom;
+    }
+    
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("The rate ages to read This KidsMagazine is " + GetAgeReccom() + " Years");
+    }
+}
